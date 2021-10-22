ce1db871f8396253e051d5ddfc1aa41bc2d999d6
Josue Morales <josuemral10@gmail.com>
2021-10-21 23:27:59 -0600

    Estructura del Pom y lo principal de Figura

---
+++ src/main/java/org/josue/Figura.java
@@ -0,0 +1,12 @@
+package org.josue;
+import java.awt.*;
+
+public abstract class Figura {
+    int lado = 0;
+    public Figura(int lado) {
+        this.lado = lado;
+    }
+    public abstract double getArea();
+    public abstract void dibujar(Graphics g, int x, int y);
+
+}
